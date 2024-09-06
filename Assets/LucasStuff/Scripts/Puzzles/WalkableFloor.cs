f521dbd29c21a30185adefdee52a7343e5a9ac53
LCarrillo13 <78832132+LCarrillo13@users.noreply.github.com>
2024-09-06 01:32:04 -0700

    uhh messed around with level design n stuff

--- Assets/LucasStuff/Scripts/Puzzles/WalkableFloor.cs
+++ Assets/LucasStuff/Scripts/Puzzles/WalkableFloor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FurryChar : MonoBehaviour
+public class WalkableFloor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
